c1cad82aefb39f25f57f873bbe7ae7f1e848ae24
Fatai <kenkufatai@gmail.com>
2025-07-12 21:42:09 +0100

    Add WelcomeMessage component and integrate it in App.jsx

--- alx-react-app/src/MainContent.jsx
+++ alx-react-app/src/MainContent.jsx
@@ -9,32 +9,3 @@ function MainContent() {
 }
 
 export default MainContent;
-
-import React from 'react';
-
-function Footer() {
-  return (
-    <footer>
-      <p>© 2023 City Lovers</p>
-    </footer>
-  );
-}
-
-export default Footer;
-
-import React from 'react';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-
-function App() {
-  return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
-  );
-}
-
-export default App;
--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -2,26 +2,11 @@
   <h1>My Favorite Cities</h1>
 </header>
 
-<main>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
-</main>
 
-<footer>
-  <p>© 2023 City Lovers</p>
-</footer>
 
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-
-function App() {
-  return (
     <>
       <Header />
-      <MainContent />
-      <Footer />
-    </>
-  );
+     
 }
 
 export default App;
