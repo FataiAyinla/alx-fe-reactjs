9108a78d46f0defa96b9b6425373bb1142de8105
Fatai <kenkufatai@gmail.com>
2025-07-14 00:51:24 +0100

    Create Specific Components in a React Application

--- alx-react-app/src/MainContent.jsx
+++ alx-react-app/src/MainContent.jsx
@@ -1,4 +1,3 @@
-// src/components/MainContent.jsx
 import React from 'react';
 
 function MainContent() {
@@ -10,20 +9,3 @@ function MainContent() {
 }
 
 export default MainContent;
-
-import React from 'react';
-import Header from './components/Header';
-import MainContent from './components/MainContent'; // ✅ Required!
-import Footer from './components/Footer';
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
