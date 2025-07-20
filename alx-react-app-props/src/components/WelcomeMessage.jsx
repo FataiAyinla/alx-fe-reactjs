880f57b5ed0cb1c83121bb5084c7d6a4516c515f
Fatai <kenkufatai@gmail.com>
2025-07-20 19:37:28 +0100

    Refactor: Replace prop drilling with Context API

---
+++ alx-react-app-props/src/components/WelcomeMessage.jsx
@@ -0,0 +1,25 @@
+function WelcomeMessage() {
+    return (function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
+
+        
+
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
+
