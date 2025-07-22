4cd7b9e90d780afc97b359388f2cb8f56d9bbafc
Fatai <kenkufatai@gmail.com>
2025-07-22 18:26:37 +0100

    Fix: Add UserProfile component using useContext and UserContext

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const UserProfile = (props) => {
+const userProfile = (props) => {
   return (
     <div> style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
       <h2>{props.name}</h2>
@@ -10,4 +10,19 @@ const UserProfile = (props) => {
   );
 };
 
-  
+  import React, { useContext } from 'react';
+import userContext from '../UserContext';
+
+const UserProfile = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2>{user.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{user.age}</span></p>
+      <p>Bio: {user.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
