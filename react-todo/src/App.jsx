6ab0daafd4bfa4a2db980991f59a383c1aa7001a
Fatai <kenkufatai@gmail.com>
2025-08-21 18:39:34 +0100

    Add App.jsx and update index.js to render TodoList

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -23,3 +23,14 @@ function App() {
 }
 
 export default App;
+
+import React from "react";
+import TodoList from "./components/TodoList";
+
+export default function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
