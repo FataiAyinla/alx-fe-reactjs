399c48cb11d36ca4957d53cc1638bd1def508fae
Fatai <kenkufatai@gmail.com>
2025-08-24 23:20:05 +0100

    Implement controlled form and Formik form with Yup validation

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,5 @@
-import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/FormikForm";
+import RegistrationForm from "./components/RegistrationForm.jsx";
+import FormikForm from "./components/formikform";
 
 function App() {
   return (
