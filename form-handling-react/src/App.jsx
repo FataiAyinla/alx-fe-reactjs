ff4bba3fdc570ccc9b737994ddc9c5966dbe50ee
Fatai <kenkufatai@gmail.com>
2025-08-24 23:36:16 +0100

    Add FormikForm component with Yup validation

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,10 @@
 import FormikForm from "./components/formikForm";
 
+function App() {
+  return <FormikForm />;
+}
+
+
 import React from "react";
 import RegistrationForm from "./components/RegistrationForm";
 import FormikForm from "./components/formikForm"; // lowercase file
